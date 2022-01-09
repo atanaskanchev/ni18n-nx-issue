30122d7524d63e2a046f8c784004a430ee19ae3f
Atanas Kanchev <atanas.kanchev@valens-services.co.uk>
2022-01-09 20:23:30 +0000

    initial commit

---
+++ apps/ni18n-nx-issue/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'ni18n-nx-issue',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/ni18n-nx-issue',
+};
